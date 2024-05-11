0973124b38214f08aa4d849a95e5bb6dbf076100
ApacheThunder <spenserwhaley@gmail.com>
2024-05-10 19:02:41 -0500

    Fix shaders for Windows
    * Fix mistake that would break custom shaders on anything except Linux.
    Forgot to remove test code when adding new Linux asset bundle. :P

--- ExpandTheGungeon/ExpandTheGungeon.cs
+++ ExpandTheGungeon/ExpandTheGungeon.cs
@@ -24,7 +24,7 @@ namespace ExpandTheGungeon {
 
         public const string GUID = "ApacheThunder.etg.ExpandTheGungeon";
         public const string ModName = "ExpandTheGungeon";
-        public const string VERSION = "2.9.15";
+        public const string VERSION = "2.9.16";
         public static string ZipFilePath;
         public static string FilePath;
         public static string ResourcesPath;
@@ -107,9 +107,7 @@ namespace ExpandTheGungeon {
             if (Application.platform == RuntimePlatform.LinuxPlayer | Application.platform == RuntimePlatform.OSXPlayer) {
                 ModAssetBundleName = "ExpandSharedAuto_Linux";
             }
-
-            ModAssetBundleName = "ExpandSharedAuto_Linux";
-
+            
             ExpandAssets.InitCustomAssetBundles(ModName);            
                         
             ETGModMainBehaviour.WaitForGameManagerStart(GMStart);
