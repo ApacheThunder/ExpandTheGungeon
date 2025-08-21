915d54b3afbbbce0a9fe67ca80f68b7d4c3bd86c
ApacheThunder <spenserwhaley@gmail.com>
2025-08-21 16:49:17 -0500

    Updated Ammonomicon Entry ...
    * Updated Ammonomicon entriy for new enemy. Spritework courtasy of
    Dallan.

--- ExpandTheGungeon/ExpandTheGungeon.cs
+++ ExpandTheGungeon/ExpandTheGungeon.cs
@@ -24,7 +24,7 @@ namespace ExpandTheGungeon {
 
         public const string GUID = "ApacheThunder.etg.ExpandTheGungeon";
         public const string ModName = "ExpandTheGungeon";
-        public const string VERSION = "2.10.2";
+        public const string VERSION = "2.11.0";
         public static string ZipFilePath;
         public static string FilePath;
         public static string ResourcesPath;
